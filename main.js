cc64f531c24cac0fc6c2de2e7f720ea86a2e97f2
Sierra <55088939+evelynsierra@users.noreply.github.com>
2021-12-10 09:30:12 +0700

    Challenge #6

--- main.js
+++ main.js
@@ -196,6 +196,89 @@ var uNormalModel = gl.getUniformLocation(shaderProgram, "uNormalModel");
 var uViewerPosition = gl.getUniformLocation(shaderProgram, "uViewerPosition");
 gl.uniform3fv(uViewerPosition, camera);
 
+//untuk putar
+var lastPointOnTrackBall, currentPointOnTrackBall;
+var lastQuat = glMatrix.quat.create();
+function computeCurrentQuat() {
+    // Secara berkala hitung quaternion rotasi setiap ada perubahan posisi titik pointer mouse
+    var axisFromCrossProduct = glMatrix.vec3.cross(glMatrix.vec3.create(), lastPointOnTrackBall, currentPointOnTrackBall);
+    var angleFromDotProduct = Math.acos(glMatrix.vec3.dot(lastPointOnTrackBall, currentPointOnTrackBall));
+    var rotationQuat = glMatrix.quat.setAxisAngle(glMatrix.quat.create(), axisFromCrossProduct, angleFromDotProduct);
+    glMatrix.quat.normalize(rotationQuat, rotationQuat);
+    return glMatrix.quat.multiply(glMatrix.quat.create(), rotationQuat, lastQuat);
+}
+// Memproyeksikan pointer mouse agar jatuh ke permukaan ke virtual trackball
+function getProjectionPointOnSurface(point) {
+    var radius = canvas.width/3;  // Jari-jari virtual trackball kita tentukan sebesar 1/3 lebar kanvas
+    var center = glMatrix.vec3.fromValues(canvas.width/2, canvas.height/2, 0);  // Titik tengah virtual trackball
+    var pointVector = glMatrix.vec3.subtract(glMatrix.vec3.create(), point, center);
+    pointVector[1] = pointVector[1] * (-1); // Flip nilai y, karena koordinat piksel makin ke bawah makin besar
+    var radius2 = radius * radius;
+    var length2 = pointVector[0] * pointVector[0] + pointVector[1] * pointVector[1];
+    if (length2 <= radius2) pointVector[2] = Math.sqrt(radius2 - length2); // Dapatkan nilai z melalui rumus Pytagoras
+    else {  // Atur nilai z sebagai 0, lalu x dan y sebagai paduan Pytagoras yang membentuk sisi miring sepanjang radius
+        pointVector[0] *= radius / Math.sqrt(length2);
+        pointVector[1] *= radius / Math.sqrt(length2);
+        pointVector[2] = 0;
+    }
+    return glMatrix.vec3.normalize(glMatrix.vec3.create(), pointVector);
+}
+
+var dragging, rotation = glMatrix.mat4.create();
+
+function onMouseDown(event) { //saat mouse di drag ke bawah
+    var x = event.clientX;
+    var y = event.clientY;
+    var rect = event.target.getBoundingClientRect();
+
+    if(
+        rect.left <= x &&
+        rect.right >= x &&
+        rect.top <= y &&
+        rect.bottom >= y
+    ) {
+        dragging = true;
+    }
+    lastPointOnTrackBall = getProjectionPointOnSurface(glMatrix.vec3.fromValues(x,y,0));
+    currentPointOnTrackBall = lastPointOnTrackBall;
+}
+
+function onMouseUp(event){
+    dragging = false;
+    if(currentPointOnTrackBall != lastPointOnTrackBall){
+        lastQuat = computeCurrentQuat();
+    }
+}
+
+function onMouseMove(event) {
+    if (dragging){
+        var x = event.clientX;
+        var y = event.clientY;
+        currentPointOnTrackBall = getProjectionPointOnSurface(glMatrix.vec3.fromValues(x,y,0));
+        glMatrix.mat4.fromQuat(rotation,computeCurrentQuat());
+
+    //     var xaxis = [1,0,0,0];
+    //     var yaxis = [0,1,0,0];
+    //     var inverseRotation = glMatrix.mat4.create();
+        
+    //     glMatrix.mat4.invert(inverseRotation, rotation);
+    //     glMatrix.vec4.transformMat4(xaxis, xaxis, inverseRotation);
+    //     glMatrix.vec4.transformMat4(yaxis, yaxis, inverseRotation);
+
+    //     var dx = (x - lastx)/60;
+    //     var dy = (y - lasty)/60;
+    //     var radx = glMatrix.glMatrix.toRadian(dy);
+    //     var rady = glMatrix.glMatrix.toRadian(dx);
+    //     glMatrix.mat4.rotate(rotation, rotation, radx, xaxis);
+    //     glMatrix.mat4.rotate(rotation, rotation, rady, yaxis);
+    // }
+}
+}
+
+document.addEventListener("mousedown", onMouseDown, false);
+document.addEventListener("mouseup", onMouseUp, false);
+document.addEventListener("mousemove", onMouseMove, false);
+
 var cubes = [...cubeLight];
 
 //BUKA LAMPU
@@ -218,13 +301,17 @@ function render() {
         vertices = [...penghapusKanan, ...cubes, ...penghapusKiri, ...planeVertices,];
         gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+        gl.uniform3fv(uLightPosition, lightPosition);
        // Init the model matrix
        var model = glMatrix.mat4.create();
+       glMatrix.mat4.multiply(model, model, rotation);
        gl.uniformMatrix4fv(uModel, false, model);
+
        // Set the model matrix for normal vector
        var normalModel = glMatrix.mat3.create();
        glMatrix.mat3.normalFromMat4(normalModel, model);
        gl.uniformMatrix3fv(uNormalModel, false, normalModel);
+
        // Reset the frame buffer
        gl.enable(gl.DEPTH_TEST);
        gl.clearColor(0.52, 0.1, 0.1, 1.0);
